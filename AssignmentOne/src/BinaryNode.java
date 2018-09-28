16f77e2e8ce8cb2607b7a393b7ec4289e14e23e0
endam1234 <mckennen@tcd.ie>
2018-09-28 13:09:46 +0100

    Commit

---
+++ AssignmentOne/src/BinaryNode.java
@@ -0,0 +1,11 @@
+
+// Binary Tree node 
+public class BinaryNode { 
+    int data; 
+    BinaryNode left, right; 
+  
+    BinaryNode(int value) { 
+        data = value; 
+        left = right = null; 
+    } 
+} 
