553b11f8079cddef843018863da0c8ba05a22997
endam1234 <mckennen@tcd.ie>
2018-10-02 16:17:34 +0100

    Updated tests

--- LCAVersion1/src/LowestCommonAncestorTest.java
+++ LCAVersion1/src/LowestCommonAncestorTest.java
@@ -17,6 +17,30 @@ public class LowestCommonAncestorTest {
 		assertEquals("Find LCA when tree is empty: ", null, tree.findLCA(1, 2) );
 	}
 
+	@Test
+	public void testTwoNodes(){
+		LCA tree = new LCA();
+		tree.root = new BinaryNode(1);
+		tree.root.left = new BinaryNode(2);
+		assertEquals("Find LCA of the tree with only root and one child: ", 1, tree.findLCA(1, 2).data);	
+	}
+
+	@Test
+	public void testForNonExistentNodes(){
+
+		LCA tree = new LCA();
+
+		tree.root = new BinaryNode(1);
+		tree.root.left = new BinaryNode(2);
+		tree.root.right = new BinaryNode(3);
+		tree.root.left.left = new BinaryNode(4);
+		tree.root.left.right = new BinaryNode(5);
+		tree.root.right.left = new BinaryNode(6);
+		tree.root.right.right = new BinaryNode(7);
+		assertEquals("Find ancestors of non-existent nodes in a populated tree: ", null, tree.findLCA(8, 9));
+		assertEquals("Find ancestors of non-existent nodes in a populated tree: ", null, tree.findLCA(32, 76));
+	}	
 	
+
 }
 
